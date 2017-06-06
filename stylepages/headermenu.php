333215df0bca5179894f7d540b910979a1cb58a8
Peskit <lschweiber@gmail.com>
2017-06-06 22:13:44 +0200

    Basic Content

---
+++ stylepages/headermenu.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Header - Menu</title>
+		<link href="../css/layout.css" rel="stylesheet" type="text/css" media="screen"/>
+		<link href="../css/header.css" rel="stylesheet" type="text/css" media="screen"/>
+	</head>
+
+	<body>
+		<table><td>
+		<img src="../images/header/hl-0<?php echo(mt_rand(1,4)); ?>.png" height="200px">
+		</td><td>
+		<img src="../images/spacer.png" width="50px">
+		</td><td>
+		<menubtn><a href="../index.html" target="content">Home</a></menubtn> | <menubtn><a href="../recipes.php" target="content">Filme</a></menubtn> | <menubtn><a href="../impressum.xml" target="content">Impressum</a></menubtn>
+		<h4>Persoenlicher Bereich</h4>
+		<menubtn><a href="../profile.php" target="content">Profil</a></menubtn>
+		</td>
+	</body>
+</html>
