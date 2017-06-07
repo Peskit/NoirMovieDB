865baffb8943270c19c9992b215ae53dd7242876
Peskit <lschweiber@gmail.com>
2017-06-07 20:26:52 +0200

    Update headermenu.php

--- stylepages/headermenu.php
+++ stylepages/headermenu.php
@@ -15,7 +15,7 @@
 		</td><td>
 		<a href="../index.html" target="_parent"><btn>Home</btn></a> | <a href="../movies.html" target="_parent"><btn>Alle Filme</btn></a> | <a href="../newMovie.html" target="_parent"><btn>Neuer Film</btn></a>
 		<h4>Persoenlicher Bereich</h4>
-		<a href="../profile.php" target="_parent"><btn>Profil</btn></a>
+		<a href="../profile.html" target="_parent"><btn>Profil</btn></a>
 		</td></table>
 	</body>
 </html>
